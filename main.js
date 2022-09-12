b39877a6c3532628efd37a48046d48bc5c73188a
Lazy007 <105008830+Lazy007@users.noreply.github.com>
2022-09-13 00:03:10 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+document.getElementsByTagName('img')[0].textContent='dark_mode';
+function myFunction() {
+   var element = document.getElementsByTagName('body')[0];   
+   element.classList.toggle("dark_mode");
+   
+   var button = document.getElementsByTagName('img')[0];
+   if(img.textContent == 'dark_mode')img.textContent='white_mode';
+   else else.textContent='dark_mode';
+}
