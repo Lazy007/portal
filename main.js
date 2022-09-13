b329218e84a966d47d2ae8324d8a9cfcdfc7be7f
Lazy007 <105008830+Lazy007@users.noreply.github.com>
2022-09-13 14:57:27 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-document.getElementsByTagName('img')[0].textContent='dark_mode';
+document.getElementsByTagName('a')[0].textContent='dark_mode';
 function myFunction() {
    var element = document.getElementsByTagName('body')[0];   
    element.classList.toggle("dark_mode");
    
-   var button = document.getElementsByTagName('img')[0];
-   if(img.textContent == 'dark_mode')img.textContent='white_mode';
+   var button = document.getElementsByTagName('a')[0];
+   if(a.textContent == 'dark_mode')a.textContent='white_mode';
    else else.textContent='dark_mode';
 }
