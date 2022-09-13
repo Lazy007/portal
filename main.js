cb54e163df1e5b5f7cc847c39692256e21f68c0c
Lazy007 <105008830+Lazy007@users.noreply.github.com>
2022-09-13 17:41:08 +0530

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-document.getElementsByTagName('a')[0].textContent='dark_mode';
-function myFunction() {
-   var element = document.getElementsByTagName('body')[0];   
-   element.classList.toggle("dark_mode");
-   
-   var a = document.getElementsByTagName('a')[0];
-   if(a.textContent == 'dark_mode')a.textContent='white_mode';
-   else else.textContent='dark_mode';
-}
